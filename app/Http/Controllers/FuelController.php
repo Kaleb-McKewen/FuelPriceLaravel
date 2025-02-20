447deeb1cd5f2d851a13289d53f7fb88cf5f1a9c
usc-KWM007 <KWM007@student.usc.edu.au>
2025-02-20 17:59:29 +1000

    Reworked DB Schedule based on new API
    Calls API and formats correctly based on public API output, gets correct fuel type, fuel station names, lowest five and average price.

--- app/Http/Controllers/FuelController.php
+++ app/Http/Controllers/FuelController.php
@@ -8,24 +8,26 @@ use Illuminate\Validation\Rule;
 
 class FuelController extends Controller
 {
-    public function index(){
+    public function index()
+    {
 
         $fuelprice = Fuelprice::all();
         $times = "";
         $average_prices = "";
-        foreach($fuelprice as $entry){
-            $times .= $entry->time.',';
-            $average_prices .=$entry->average.',';
+        foreach ($fuelprice as $entry) {
+            $times .= $entry->time . ',';
+            $average_prices .= $entry->average . ',';
         }
         $latestFuelpriceRaw = Fuelprice::orderby('time', 'desc')->first()->lowest;
 
         $latestFuelprice = preg_split("/\r\n|\n|\r/", $latestFuelpriceRaw);
         array_pop($latestFuelprice);
-        
+
         return view('components.all', compact('times', 'average_prices', 'latestFuelprice'));
     }
 
-    public function rawData(Request $request){
+    public function rawData(Request $request)
+    {
         //allowed values for validation
         $allowedColumns = ['time', 'average'];
         $allowedOrders = ['asc', 'desc'];
@@ -35,12 +37,12 @@ class FuelController extends Controller
             'order' => Rule::in($allowedOrders),
         ]);
 
-        $fuelprices = Fuelprice::select('id','time','average')->when(request('sort'), function ($query) {
+        $fuelprices = Fuelprice::select('id', 'time', 'average')->when(request('sort'), function ($query) {
             return $query->orderby(request('sort'), request('order'));
-        }, function($query){
+        }, function ($query) {
             return $query->orderby('time', 'desc'); //default
         })->paginate(15);
-        
+
         return view('components.rawData', compact('fuelprices'));
     }
 }
--- routes/console.php
+++ routes/console.php
@@ -1,4 +1,5 @@
 <?php
+
 use Illuminate\Support\Facades\Http;
 use App\Models\Fuelprice;
 use Illuminate\Foundation\Inspiring;
@@ -10,18 +11,81 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
 
-Schedule::call(function(){
-    //API GET
-    $response = json_decode(Http::get(env('URL')));
+
+//Used to get newest data from API and commit to DB
+Schedule::call(function () {
+    //todays date
+    $date = new DateTime;
+    $average = [];
+
+    //change verify in production, only bypassed
+    //station names
+    $fuelStations = json_decode(Http::withOptions(['verify' => false])->withHeader("Authorization", env("API_AUTHORIZATION"))->get(env("FUEL_STATIONS_URL")));
+    //station prices
+    $allFuelPrices = json_decode(Http::withOptions(['verify' => false])->withHeader("Authorization", env("API_AUTHORIZATION"))->get(env("FUEL_PRICE_URL")));
+
+    //filter for fuel type and outdated
+    $filteredFuelPrices = array_filter($allFuelPrices->SitePrices, function ($obj) use (&$average, $date) {
+        if (isset($obj->FuelId)) {
+            $fuelDate = new DateTime($obj->TransactionDateUtc);
+            $dateDiff = $fuelDate->diff($date)->format("%a");
+            //less than a two days old
+            if ($dateDiff > 2) {
+                return false;
+            }
+            //fuel type of 95
+            if ($obj->FuelId != 5) {
+                return false;
+            }
+            //add to average
+            array_push($average, $obj->Price);
+            return true;
+        }
+    });
+
+    //get average
+    $average = round((array_sum($average) / count($average)) / 10, 2);
+
+    //sort by cheapest
+    usort($filteredFuelPrices, function ($obj1, $obj2) {
+        return strcmp($obj1->Price, $obj2->Price);
+    });
+
+    //get lowest five stations
+    $lowestFive = array_slice($filteredFuelPrices, 0, 5, true);
+
+    $lowest = "";
+    //get names for lowest five
+    foreach ($lowestFive as $fuelStation) {
+        foreach ($fuelStations->S as $stationData) {
+            if ($stationData->S == $fuelStation->SiteId) {
+                //format date difference string
+                $dateChange = "";
+                $fuelDate = new DateTime($fuelStation->TransactionDateUtc);
+                $dateDiff = $fuelDate->diff($date);
+                if ($dateDiff->format('%a') > 1) {
+                    $dateChange = $dateDiff->format('%a') . " days ago";
+                } else if ($dateDiff->format('%h') > 1) {
+                    $dateChange = $dateDiff->format('%h') . " hours ago";
+                } else {
+                    $dateChange = $dateDiff->format('%i') . " mins ago";
+                }
+                //format lowest string
+                $lowest = $lowest . $stationData->N . ' ' . ($fuelStation->Price / 10) . ' ' . $dateChange . "\r\n";
+            };
+        }
+    }
+    //dd($lowest);
+
+
     //Save new entry to DB
     $fuelEntry = new Fuelprice();
-    $fuelEntry->time = $response->time;
-    $fuelEntry->average = $response->average;
-    $fuelEntry->lowest = $response->lowest;
+    $fuelEntry->time = $date->format("Y-m-d");
+    $fuelEntry->average = $average;
+    $fuelEntry->lowest = $lowest;
     $fuelEntry->save();
-    //update frequency
+    //frequency every 6 hours
 })->everySixHours();
-
 
 /*
 #Test schedule
