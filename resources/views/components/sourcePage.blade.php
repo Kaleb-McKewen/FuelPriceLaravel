23d8788bd4af02d30eed385c9f8c473ea7f990f1
usc-KWM007 <KWM007@student.usc.edu.au>
2025-02-20 20:34:08 +1000

    Added API reference
    Added Fuel Price API reference

--- resources/views/components/sourcePage.blade.php
+++ resources/views/components/sourcePage.blade.php
@@ -1,9 +1,12 @@
 <x-layout>
 
-    <div class="flex flex-col mx-8">
-        <div class="flex justify-center mb-4">
+    <div class="flex flex-col mx-8 items-center flex-wrap">
+        <div class="mb-4">
             <h1 class="text-3xl font-medium underline">Data Source Credits</h1>
             <!--Add Source Attributions from API-->
         </div>
-
+        <div class="w-2xl">
+            <p class="text-center">{{ env("API_REFERENCE") }}</p>
+        </div>
+    </div>
 </x-layout>
